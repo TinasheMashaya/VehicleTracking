b7a576ab985895abb99c6045670da6fbc284cb60
Tinashe Mashaya <tinashemashaya18@gmail.com>
2023-01-27 17:30:27 +0200

    added dockerfile

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM php:7.2-cli
+# COPY . /usr/src/myapp
+# WORKDIR /usr/src/myapp
+COPY config /config
+COPY model /model
+COPY FetchVehicles /FetchVehicles
+# CMD [ "php", "./index.php" ]
+CMD [ "php", "-S 0.0.0.0:8280"]
+# , "./index.php" ]
--- config/DatabaseHelper.php
+++ config/DatabaseHelper.php
@@ -1,10 +1,10 @@
 <?php
 class DatabaseHelper {
     // DB Params
-    private $host_name = 'localhost';
-    private $database_name = 'vehicleDB';
+    private $host_name = 'vehicledb-ueanvc:3306';
+    private $database_name = 'vehiclesDB';
     private $username = 'tinashe';
-    private $password = '1234';
+    private $password = 'tinashe123';
     private $database_connection;
 
     // DB Connect
@@ -12,7 +12,7 @@ class DatabaseHelper {
         $this->database_connection = null;
 
         try {
-            $this->database_connection = new PDO('mysql:host=' . $this->host_name . ';dbname=' . $this->database_name, $this->username, $this->password);
+            $this->database_connection = new PDO('mysql:host=' . $this->host_name . ';dbname=' . $this->database_name.';port=3306', $this->username, $this->password);
             $this->database_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $e) {
             echo 'Failed To Connect: ' . $e->getMessage();
